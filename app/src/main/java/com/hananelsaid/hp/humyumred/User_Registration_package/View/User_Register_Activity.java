419fcb5c028b536c7b7651f6f9d903242930fc0b
hanan012 <helsaid13@gmail.com>
2019-02-20 16:53:33 +0200

    before coding user register

---
+++ app/src/main/java/com/hananelsaid/hp/humyumred/User_Registration_package/View/User_Register_Activity.java
@@ -0,0 +1,13 @@
+package com.hananelsaid.hp.humyumred.User_Registration_package.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
